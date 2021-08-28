ffc41233ddfd6245f80874e4d240d38a82109374
Cat Tran <54409102+ctran20@users.noreply.github.com>
2021-08-27 22:47:57 -0400

    Set up basic screens

---
+++ screens/NewPlaceScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const NewPlaceScreen = () => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default NewPlaceScreen;
